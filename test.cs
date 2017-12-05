a0854cb944b6ed5049691fa6e3601ae797421152
Rafael Rodrigues <rafaelrds87@yahoo.com.br>
2017-12-05 20:34:55 -0200

    Add teste

---
+++ test.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+public static void Main(string[] args)
+{
+     Console.WriteLine("Hello World!");
+		if (args !=null)
+		{
+			Console.WriteLine("Args:");
+			foreach (var arg in args)
+			{
+				Console.WriteLine(arg);
+			}
+		}
+}
+}
